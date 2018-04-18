3e2e2432f226b3946913953f52248db3699ddd93
eduardpredescu <eduard.florin.predescu@gmail.com>
2018-04-18 20:28:22 +0300

    initial commit

---
+++ PDM_Xamarin/PDM_Xamarin/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDM_Xamarin
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			var button = this.FindByName<Button>("button");
+			button.Clicked += (sender, e) => button.Text = "Apasat";
+		}
+	}
+}
